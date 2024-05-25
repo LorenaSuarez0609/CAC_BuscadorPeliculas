2bc1ce8f8d9d84ce725621ac5d0c41b5162444db
Lorena <128533577+LorenaSuarez0609@users.noreply.github.com>
2024-05-25 13:06:38 -0300

    extrenos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-
-document.getElementById('myForm').addEventListener('submit',function(event) {
+document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
-
-
     if (validar(this)) {
         this.submit();
     }
@@ -13,37 +10,31 @@ function limpiarError() {
     for ( let i=0; i<errores.length; i++){
         errores[i].innerHTML = '';
     }
-    
+    limpiarError();
 }
-
-limpiarError();
 
 function validar(formulario) {
 
     var expReg = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
 
-
     if(formulario.name.value.length == 0){
-        alert('se genero registro incorrectamente')
-        document.getElementById('errorName').innerText = "completar campo";
+        alert('Su Registro es incorrecto, faltan datos o datos incorrectos en Nombre');
+        document.getElementById('errorName').innerText = "Completar Campo";
         formulario.name.focus();
         return false
-        
+    } else{
+        document.getElementById('errorName').innerText = "Campo Correcto";
     }
-    else{
-        document.getElementById('errorName').innerText = "campo correcto";
-    }
+
     if(formulario.lastName.value.length == 0){
-        alert('se genero registro incorrectamente')
+        alert('Su Registro es incorrecto, faltan datos o datos incorrectos en apellido')
         document.getElementById('errorLastName').innerText = "completar campo";
         formulario.lastName.focus();
         return false
-        
     }else{
-        document.getElementById('errorLastName').innerText = "campo correcto";
+        document.getElementById('errorLastName').innerText = "Campo Correcto";
     }
 
-   
     if(!expReg.test(formulario.email.value)) {
         document.getElementById('errorEmail').innerText = "ingresar un email valido";
         formulario.email.focus();
@@ -51,16 +42,16 @@ function validar(formulario) {
     }
 
     if(formulario.password.value.trim().length ==0) {
-        document.getElementById('errorPassword').innerText = 'ingresar una contraseña valida';
+        document.getElementById('errorPassword').innerText = 'Ingresar una contraseña valida';
         formulario.password.focus();
         return false;
     }
 
     if (formulario.password.value != formulario.confirmarPassword.value){
-        document.getElementById('confirmarErrorPassword').innerText = 'las contraseñas no son iguales';
+        document.getElementById('confirmarErrorPassword').innerText = 'Las contraseñas no son iguales';
         formulario.confirmarPassword.focus();
     }
 
-    alert('se genero registro correctamente')
+    alert('se genero registro correctamente');
     return true;
 }
